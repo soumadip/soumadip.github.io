85eda71e38b0d2f8e213c58cc6f8b71d40009f2e
Soumadip Biswas <soumadip@alucard.localdomain>
2022-10-29 21:10:42 +0530

    Saturday 29 October 2022 09:10:42 PM IST

--- I2C/res/l4/break_continue.c
+++ I2C/res/l4/break_continue.c
@@ -4,33 +4,21 @@ int main ()
 {
 	int i, j, k;
 
-	printf ("LOOP 1\n");
+	printf ("LOOP 1 using break\n");
 	for (int i = 0; i <10; i++)
 	{
+		printf ("Line before break; this is iteration number %d\n", i);
 		if (i == 6)
 			break;
-		printf ("this is iteration number %d\n", i);
+		printf ("Line after break; this is iteration number %d\n", i);
 	}
-	printf ("LOOP 2\n");
+	printf ("LOOP 2 using continue\n");
 	for (int i = 0; i <10; i++)
 	{
-		printf ("this is iteration number %d\n", i);
-		if (i == 6)
-			break;
-	}
-	printf ("LOOP 3\n");
-	for (int i = 0; i <10; i++)
-	{
+		printf ("Line before continue; this is iteration number %d\n", i);
 		if (i >= 3)
 			continue;
-		printf ("this is iteration number %d\n", i);
-	}
-	printf ("LOOP 4\n");
-	for (int i = 0; i <10; i++)
-	{
-		printf ("this is iteration number %d\n", i);
-		if (i >= 3)
-			continue;
+		printf ("Line after continue; this is iteration number %d\n", i);
 	}
 }
 
