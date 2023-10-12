63a41d426aa766ca59078d049fc6e2164d356692
Soumadip Biswas <soumadip.cse@gmail.com>
2023-10-12 10:42:03 +0530

    updates

---
+++ courses/I2C/res/l8/dma.c
@@ -0,0 +1,25 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+int main ()
+{
+	int *ptr = malloc(sizeof(int));
+
+	*ptr = 100;
+
+	printf("val : %d\n", *ptr);
+	printf("address: %p\n", ptr);
+
+	printf("Enter new value : ");
+	scanf("%d", ptr);
+	int *arr = malloc(*ptr * sizeof(int));
+
+	for(int i=0; i <*ptr; i++)
+		scanf("%d", &arr[i]);
+
+	for(int i=0; i <*ptr; i++)
+		printf("[%p] %d\n", arr+i, *(arr+i));
+
+}
+
